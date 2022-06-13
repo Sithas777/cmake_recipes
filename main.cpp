4a3bf8a1d6cdf76343d729fc8f044fdfd5316b5b
Anton Sysoev <sisoev.a@n-t.io>
2022-06-13 23:32:30 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::literals::string_literals::operator"" s;
+
+int
+main()
+{
+  cout << "Hello, World!"s << endl;
+
+  return EXIT_SUCCESS;
+}
