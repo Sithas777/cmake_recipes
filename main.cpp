5633561751bb0d95b2a0f95447777f0e3633a908
Anton Sysoev <sisoev.a@n-t.io>
2022-06-17 17:56:09 +0300

    recipe8 - select standard

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <string_view>
 
 #include "FizzBuzz.hpp"
 
 using std::string;
+using std::string_view;
 using std::cout;
 using std::endl;
 using std::literals::string_literals::operator""s;
+using std::literals::string_view_literals::operator""sv;
 
 int
 main()
