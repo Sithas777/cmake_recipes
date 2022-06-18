df7f0450aeb7404fb3b8077d340c6018884dd1d6
Anton Sysoev <sisoev.a@n-t.io>
2022-06-18 18:44:16 +0300

    chapter2/feature2 - conditional compilation

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
 using std::literals::string_literals::operator"" s;
+
+string
+get_greeting()
+{
+#ifdef IS_WINDOWS
+  return "Hello, Windows!"s;
+#elif IS_LINUX
+  return "Hello, Linux!"s;
+#elif IS_MACOS
+  return "Hello, MacOS!"s;
+#else
+  return "Hello from unknown system!"s;
+#endif
+}
 
 int
 main()
 {
-  cout << "Hello, World!"s << endl;
+  cout << get_greeting() << endl;
 
   return EXIT_SUCCESS;
 }
