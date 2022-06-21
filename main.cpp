0395e326a166a620baa6bb26f7760528cb804d56
Anton Sysoev <sisoev.a@n-t.io>
2022-06-21 16:31:54 +0300

    chapter2/recipe4 - add mpi (need install MPI SDK and reboot Windows)

--- main.cpp
+++ main.cpp
@@ -1,32 +1,7 @@
-#include <iostream>
-#include <omp.h>
-#include <string>
-
-using namespace std;
+#include <mpi.h>
 
 int
 main()
 {
-  cout << "Number of threads: "s << omp_get_max_threads() << endl;
-
-  long long n = 100000;
-
-  cout << "we will form sum of numbers from 1 to " << n << endl;
-
-  auto t0 = omp_get_wtime();
-
-  long long s = 0LL;
-
-#pragma omp parallel for reduction (+ : s)
-  for (auto i = 1; i <=n; ++i)
-  {
-    s += i;
-  }
-
-  auto t1 = omp_get_wtime();
-
-  cout << "sum: "s << s << endl;
-  cout << "elapsed wall clock time: "s << t1 - t0 << " seconds"s << endl;
-
-  return EXIT_SUCCESS;
+  return 0;
 }
