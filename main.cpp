f0c2a5be9ffc5d79736543ce3c643519d0cbfe2c
Anton Sysoev <sisoev.a@n-t.io>
2022-06-21 18:30:47 +0300

    chapter2/recipe5 - correctly detect Eigen

--- main.cpp
+++ main.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
-#include <omp.h>
-#include <string>
+#include <Eigen/Dense>
 
-using namespace std;
+using Eigen::MatrixXd;
 
 int
 main()
 {
-  cout << "Number of threads: "s << omp_get_max_threads() << endl;
-
-  long long n = 100000;
-
-  cout << "we will form sum of numbers from 1 to " << n << endl;
-
-  auto t0 = omp_get_wtime();
-
-  long long s = 0LL;
-
-#pragma omp parallel for reduction (+ : s)
-  for (auto i = 1; i <=n; ++i)
-  {
-    s += i;
-  }
-
-  auto t1 = omp_get_wtime();
-
-  cout << "sum: "s << s << endl;
-  cout << "elapsed wall clock time: "s << t1 - t0 << " seconds"s << endl;
-
-  return EXIT_SUCCESS;
+  MatrixXd m(2, 2);
+  m(0, 0) = 3;
+  m(1, 0) = 2.5;
+  m(0, 1) = -1;
+  m(1, 1) = m(1, 0) + m(0, 1);
+  std::cout << m << std::endl;
 }
