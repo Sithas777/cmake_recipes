8cc1febe8e848643da0e1fc299d93bf2083c253c
Anton Sysoev <sisoev.a@n-t.io>
2022-06-30 12:06:30 +0300

    chapter2/recipe6 - install boost(except filesystem)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,70 @@
+#include <string>
 #include <iostream>
-#include <Eigen/Dense>
+#include <boost/system.hpp>
+#include <boost/algorithm/algorithm.hpp>
 
-using Eigen::MatrixXd;
+using namespace std;
+using std::string_literals::operator ""s;
+
+class A
+{
+  size_t i_;
+ public:
+  A() = delete;
+
+  A(const A& a_copy) : i_(a_copy.get_i())
+  {
+    cout << "Copy constructor called"s << endl;
+  }
+
+  A(A&& a_copy) : i_(a_copy.get_i())
+  {
+    cout << "Move constructor called"s << endl;
+  }
+
+  A(size_t i) : i_(i)
+  {
+    cout << "Transform constructor called"s << endl;
+  }
+
+  [[nodiscard]]
+  size_t
+  get_i() const
+  {
+    return i_;
+  }
+
+  A&
+  operator =(const A& rhs)
+  {
+    cout << "Copy operator called"s << endl;
+    i_ = rhs.i_;
+
+    return *this;
+  }
+
+  A&
+  operator =(A&& rhs)
+  {
+    cout << "Move operator called"s << endl;
+    swap(*this, rhs);
+
+    return *this;
+  }
+
+  ~A()
+  {
+    cout << "Destructor called"s << endl;
+  }
+};
 
 int
 main()
 {
-  MatrixXd m(2, 2);
-  m(0, 0) = 3;
-  m(1, 0) = 2.5;
-  m(0, 1) = -1;
-  m(1, 1) = m(1, 0) + m(0, 1);
-  std::cout << m << std::endl;
+  size_t i = 123;
+  A a = i;
+  A a1 = a;
+  A a2 = A(i);
+
+  BOOST_ASSERT(true);
 }
